502fa323bdd8bb896fd9acffc297f64a38c52e31
hzyuyongmao <hzyuyongmao@corp.netease.com>
2019-08-08 14:17:48 +0800

    MOD:TestDomain添加testInfo方法

--- src/main/java/com/deepQAWeb/domain/TestDomain.java
+++ src/main/java/com/deepQAWeb/domain/TestDomain.java
@@ -1,5 +1,20 @@
 package com.deepQAWeb.domain;
 
 public class TestDomain {
-    
+    private String testInfo;
+
+    public String getTestInfo() {
+        return testInfo;
+    }
+
+    public void setTestInfo(String testInfo) {
+        this.testInfo = testInfo;
+    }
+
+    @Override
+    public String toString() {
+        return "TestDomain{" +
+                "testInfo='" + testInfo + '\'' +
+                '}';
+    }
 }
